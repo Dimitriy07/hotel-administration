6d3b1d5301b91bc41e7770b8a70685dbb06e2c7b
Dimitriy07 <74500066+Dimitriy07@users.noreply.github.com>
2025-01-13 07:55:20 +0000

    add pagination/add checkin functionality in booking

--- src/features/bookings/BookingTable.jsx
+++ src/features/bookings/BookingTable.jsx
@@ -6,10 +6,40 @@ import Empty from "../../ui/Empty";
 import { useBookings } from "./useBookings";
 import Spinner from "../../ui/Spinner";
 
+import Pagination from "../../ui/Pagination";
+
 function BookingTable() {
-  const { bookings, isLoading } = useBookings();
+  const { bookings, isLoading, count } = useBookings();
+
   if (isLoading) return <Spinner />;
   if (!bookings.length) return <Empty resourceName="bookings" />;
+
+  // // 1) FILTER
+  // const filterValue = searchParams.get("status") || "all";
+  // let filteredBookings;
+  // if (filterValue === "all") filteredBookings = bookings;
+  // if (filterValue === "checked-out")
+  //   filteredBookings = bookings.filter(
+  //     (booking) => booking.status === "checked-out"
+  //   );
+  // if (filterValue === "checked-in")
+  //   filteredBookings = bookings.filter(
+  //     (booking) => booking.status === "checked-in"
+  //   );
+  // if (filterValue === "unconfirmed")
+  //   filteredBookings = bookings.filter(
+  //     (booking) => booking.status === "unconfirmed"
+  //   );
+
+  // //2) SORT
+
+  // const sortValue = searchParams.get("sortBy") || "startDate-desc";
+  // const [field, direction] = sortValue.split("-");
+  // const modifier = direction === "asc" ? 1 : -1;
+  // const sortedBooking = filteredBookings.sort(
+  //   (a, b) => (a[field] - b[field]) * modifier
+  // );
+
   return (
     <Menus>
       <Table columns="0.6fr 2fr 2.4fr 1.4fr 1fr 3.2rem">
@@ -28,6 +58,9 @@ function BookingTable() {
             <BookingRow key={booking.id} booking={booking} />
           )}
         />
+        <Table.Footer>
+          <Pagination count={count} />
+        </Table.Footer>
       </Table>
     </Menus>
   );
